cd7b31939b3aeb5cdff3a0943618808eea4fefc8
AR_Abdalla <113618532+arabdalla88@users.noreply.github.com>
2023-05-23 19:20:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+
+   
+            
+
+
+
+    <?php
+
+echo "<center>";
+include("index.html");
+
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $process = $_POST['process'];
+
+
+    if ($process == "+") {
+        echo "<p>Result : </p> 
+        <div style='width: 100px ; hight: 50px ; border: blue 3px solid ; background-color: lightblue' >"
+            . $num1 + $num2 .
+            "</div>";
+    } elseif ($process == "-") {
+        echo "<p>Result : </p> 
+        <div style='width: 100px ; hight: 50px ; border: blue 3px solid ; background-color: lightblue' >"
+            . $num1 - $num2 .
+            "</div>";
+    } elseif ($process == "*") {
+        echo "<p>Result : </p> 
+        <div style='width: 100px ; hight: 50px ; border: blue 3px solid ; background-color: lightblue' >"
+            . $num1 * $num2 .
+            "</div>";
+    } elseif ($process == "/") {
+        echo "<p>Result : </p> 
+        <div style='width: 100px ; hight: 50px ; border: blue 3px solid ; background-color: lightblue' >"
+            . $num1 / $num2 .
+            "</div>";
+    }
+elseif ($process == "%") {
+        echo "<p>Result : </p> 
+        <div style='width: 100px ; hight: 50px ; border: blue 3px solid ; background-color: lightblue' >"
+            . $num1 % $num2 .
+            "</div>";
+    }
+    
+
+    
+echo "</center>";
+    ?>
+    
